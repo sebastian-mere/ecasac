1b82ab2a5968aa5bf7136cbf6c13f499536b1c18
Sebastian Mere <sebastian.mere@gmail.com>
2023-10-02 21:25:58 -0300

    correcciones 2/10

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section) {
+        var offset = section.offsetTop - 120;
+        window.scrollTo({
+            top: offset,
+            behavior: 'smooth'
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.querySelectorAll('.casac-header__nav a');
+    links.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            var targetId = link.getAttribute('href').substring(1);
+            scrollToSection(targetId);
+        });
+    });
+});
